2cb488176fcb5e884b99c87812ef42ca079b091b
Rishabh157 <rishabhgour157@gmail.com>
2022-05-25 02:28:10 +0530

    Another logic Addedd

--- ArraySum.js
+++ ArraySum.js
@@ -1,6 +1,7 @@
 // Q - Checking Sum Zero in the Array
 // arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8] => Input
 
+// O(n^2) quradict time complexity
 
 function CheckArraySum(arr) 
 {
@@ -16,5 +17,37 @@ function CheckArraySum(arr)
 }
 
 let result = CheckArraySum([-5, -4, -3, -2, 0, 2, 4, 6, 8])
-
 console.log(result)
+
+
+
+// there is another logic but this logic only work with sorted Array
+// O(n) linear time complexity
+
+function linerCheckArray(arr)
+{
+ let left = 0
+ let right = arr.length - 1
+
+ while(left < right)
+ {
+  sum = arr[left] + arr[right]
+
+  if(sum === 0)
+  {
+   return [arr[left], arr[right]]
+  }
+  else if(sum > 0)
+  {
+   right--;
+  }
+  else
+  {
+   left++;
+  }
+ }
+
+}
+
+let ans = linerCheckArray([-5, -4, -3, -2, 0, 2, 4, 6, 8])
+console.log(ans)
