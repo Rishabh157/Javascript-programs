6c8405ba3926ea4f8289051e952de29295db33f2
Rishabh157 <rishabhgour157@gmail.com>
2024-06-24 02:00:53 +0530

    add the vowel length of string

--- countVowels.js
+++ countVowels.js
@@ -12,6 +12,21 @@ function countVowels(string) {
 }
 
 let result = countVowels('The only thing we have to fear is fear itself.')
-console.log(result)
+console.log(result) // 14
 
+function lenOfVowels(string) {
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let len = 0
+    for (let ele of string) {
+        if (vowels.includes(ele)) {
+            len++
+        }
+    }
+    return len
+}
+
+console.log(lenOfVowels('my pyx')); // 0
+console.log(lenOfVowels('pear tree')); // 4
+console.log(lenOfVowels('abracadabra')); // 5
+console.log(lenOfVowels('o a kak ushakov lil vo kashu kakao')); // 13
 
